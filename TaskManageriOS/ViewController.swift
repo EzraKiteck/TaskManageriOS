d33151c2da03beaa01e409bb8540374ca08c8a5e
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-11-26 10:24:59 -0600

    Initial Commit

---
+++ TaskManageriOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManageriOS
+//
+//  Created by Ezra Kiteck on 11/26/18.
+//  Copyright © 2018 Ezra Kiteck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
